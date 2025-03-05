4cad93463f22b0cc4a1958882792a24f3c218648
Zerolegacy <>
2025-03-05 17:53:53 +0800

    Edit checkstyle

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -32,7 +32,7 @@ import seedu.address.model.tag.Tag;
 /**
  * Edits the details of an existing person in the address book.
  */
-public class    EditCommand extends Command {
+public class EditCommand extends Command {
 
     public static final String COMMAND_WORD = "edit";
 
