1e02f63c3b2bdaf452173057d319bafc72538ec8
RT0118 <56866309+RT0118@users.noreply.github.com>
2025-03-12 18:43:35 +0800

    Create tests for Lesson class

---
+++ src/test/java/seedu/address/model/lesson/LessonTest.java
@@ -0,0 +1,54 @@
+package seedu.address.model.lesson;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+public class LessonTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Lesson(null, null));
+    }
+
+    @Test
+    public void constructor_invalidAddress_throwsIllegalArgumentException() {
+        String invalidModuleName = "";
+        LocalDateTime validDateTime = LocalDateTime.of(2025, 12, 12, 12, 0);
+        assertThrows(IllegalArgumentException.class, () -> new Lesson(invalidModuleName, validDateTime));
+    }
+
+    @Test
+    public void isValidModuleName() {
+        // null address
+        assertThrows(NullPointerException.class, () -> Lesson.isValidModuleName(null));
+
+        // invalid addresses
+        assertFalse(Lesson.isValidModuleName("")); // empty string
+        assertFalse(Lesson.isValidModuleName(" ")); // spaces only
+        assertFalse(Lesson.isValidModuleName("Bad-Module")); // bad chars included
+
+        // valid addresses
+        assertTrue(Lesson.isValidModuleName("Maths")); // single words
+        assertTrue(Lesson.isValidModuleName("Elementary Mathematics")); // with spaces
+    }
+
+    @Test
+    public void isValidLessonDate() {
+        // null string
+        assertThrows(NullPointerException.class, () -> Lesson.isValidLessonDate(null));
+
+        // invalid date string
+        assertFalse(Lesson.isValidLessonDate(""));
+        assertFalse(Lesson.isValidLessonDate(" "));
+        assertFalse(Lesson.isValidLessonDate("not a date"));
+        assertFalse(Lesson.isValidLessonDate("2024-12-12 12:00:00"));
+
+        // valid date string
+        assertTrue(Lesson.isValidLessonDate("2024-12-12T12:00:00"));
+    }
+}
