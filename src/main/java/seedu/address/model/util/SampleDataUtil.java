79f80442081534521b6d5b4e0c7b86030beb5e11
RT0118 <56866309+RT0118@users.noreply.github.com>
2025-03-12 18:20:39 +0800

    Update SampleDataUtil

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -21,41 +21,28 @@ import seedu.address.model.tag.Tag;
  */
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
+        LocalDateTime sampleDatetime = LocalDateTime.of(2025, 5, 12, 12, 0);
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("friends"),
-                List.of(new Lesson("Chemistry",
-                        LocalDateTime.of(2025, 5, 15, 13, 45)))),
+                new Address("Blk 30 Geylang Street 29, #06-40"), getTagSet("friends"),
+                    List.of(new Lesson("Chemistry", sampleDatetime))),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("colleagues", "friends"),
-                List.of(new Lesson("Elementary Mathematics",
-                            LocalDateTime.of(2025, 5, 12, 12, 0)),
-                        new Lesson("Advanced Mathematics",
-                                LocalDateTime.of(2025, 5, 12, 12, 0)))),
+                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), getTagSet("colleagues", "friends"),
+                List.of(new Lesson("Elementary Mathematics", sampleDatetime),
+                        new Lesson("Advanced Mathematics", sampleDatetime))),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getTagSet("neighbours"),
-                List.of(new Lesson("Biology",
-                        LocalDateTime.of(2025, 5, 14, 12, 0)),
-                        new Lesson("Physics",
-                                LocalDateTime.of(2025, 5, 14, 15, 0)))),
+                getTagSet("neighbours"), List.of(new Lesson("Biology", sampleDatetime),
+                        new Lesson("Physics", sampleDatetime))),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("family"),
-                List.of(new Lesson("Chemistry",
-                        LocalDateTime.of(2025, 6, 1, 18, 0)))),
+                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), getTagSet("family"),
+                List.of(new Lesson("Chemistry", sampleDatetime))),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"),
-                getTagSet("classmates"),
-                List.of(new Lesson("Chemistry",
-                        LocalDateTime.of(2025, 6, 16, 14, 0)))),
+                new Address("Blk 47 Tampines Street 20, #17-35"), getTagSet("classmates"),
+                List.of(new Lesson("Chemistry", sampleDatetime))),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("colleagues"),
-                List.of(new Lesson("Chemistry",
-                        LocalDateTime.of(2025, 7, 24, 12, 0))))
+                getTagSet("colleagues"), List.of(new Lesson("Chemistry", sampleDatetime)))
         };
     }
 
