f4edfea8a04595e904347cc6716f0f62b7ca26c2
bryonk <116611262+bryonk@users.noreply.github.com>
2025-03-20 16:56:25 +0800

    Update format to print lesson details

--- src/main/java/seedu/tuitionbook/model/util/SampleDataUtil.java
+++ src/main/java/seedu/tuitionbook/model/util/SampleDataUtil.java
@@ -22,6 +22,7 @@ import seedu.tuitionbook.model.tag.Tag;
 public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         LocalDateTime sampleDatetime = LocalDateTime.of(2025, 5, 12, 12, 0);
+        LocalDateTime sampleDatetime2 = LocalDateTime.of(2025, 5, 13, 12, 0);
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                 new Address("Blk 30 Geylang Street 29, #06-40"), getTagSet("student"),
@@ -29,14 +30,14 @@ public class SampleDataUtil {
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), getTagSet("parent"),
                 List.of(new Lesson("Elementary Mathematics", sampleDatetime),
-                        new Lesson("Advanced Mathematics", sampleDatetime))),
+                        new Lesson("Advanced Mathematics", sampleDatetime2))),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
                 getTagSet("Parent"), List.of(new Lesson("Biology", sampleDatetime),
-                        new Lesson("Physics", sampleDatetime))),
+                        new Lesson("Physics", sampleDatetime2))),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), getTagSet("Student"),
-                List.of(new Lesson("Chemistry", sampleDatetime))),
+                List.of(new Lesson("Chemistry", sampleDatetime2))),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Address("Blk 47 Tampines Street 20, #17-35"), getTagSet("Student", "Sec4"),
                 List.of(new Lesson("Chemistry", sampleDatetime))),
