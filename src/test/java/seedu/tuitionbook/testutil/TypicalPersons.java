dcc2b487d10d523a35f516f503cb6c701089ade8
RT0118 <56866309+RT0118@users.noreply.github.com>
2025-04-07 15:19:58 +0800

    Fix bug in allow load duplicate lessons from json file

--- src/test/java/seedu/tuitionbook/testutil/TypicalPersons.java
+++ src/test/java/seedu/tuitionbook/testutil/TypicalPersons.java
@@ -11,7 +11,6 @@ import static seedu.tuitionbook.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.tuitionbook.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static seedu.tuitionbook.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,6 +18,7 @@ import java.util.List;
 import seedu.tuitionbook.model.AddressBook;
 import seedu.tuitionbook.model.lesson.Lesson;
 import seedu.tuitionbook.model.person.Person;
+import seedu.tuitionbook.model.util.SampleDataUtil;
 
 /**
  * A utility class containing a list of {@code Person} objects to be used in tests.
@@ -28,31 +28,44 @@ public class TypicalPersons {
     public static final Person ALICE = new PersonBuilder().withName("Alice Pauline")
             .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
             .withPhone("94351253")
-            .withTags("friends").build();
+            .withTags("friends")
+            .build();
     public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
             .withAddress("311, Clementi Ave 2, #02-25")
             .withEmail("johnd@example.com").withPhone("98765432")
             .withTags("owesMoney", "friends")
-            .withLessons(List.of(
-                    new Lesson("Benson Lesson",
-                            LocalDateTime.of(2025, 12, 12, 22, 0)))
-            ).build();
+            .withLessons(List.of(new Lesson("BENSON Lesson", SampleDataUtil.SAMPLE_DATE_TIME_2)))
+            .build();
     public static final Person CARL = new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-            .withEmail("heinz@example.com").withAddress("wall street").build();
+            .withEmail("heinz@example.com").withAddress("wall street")
+            .withLessons(List.of(new Lesson("CARL Lesson", SampleDataUtil.SAMPLE_DATE_TIME_3)))
+            .build();
     public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
-            .withEmail("cornelia@example.com").withAddress("10th street").withTags("friends").build();
+            .withEmail("cornelia@example.com").withAddress("10th street").withTags("friends")
+            .withLessons(List.of(new Lesson("DANIEL Lesson", SampleDataUtil.SAMPLE_DATE_TIME_4)))
+            .build();
     public static final Person ELLE = new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
-            .withEmail("werner@example.com").withAddress("michegan ave").build();
+            .withEmail("werner@example.com").withAddress("michegan ave")
+            .withLessons(List.of(new Lesson("ELLE Lesson", SampleDataUtil.SAMPLE_DATE_TIME_5)))
+            .build();
     public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-            .withEmail("lydia@example.com").withAddress("little tokyo").build();
+            .withEmail("lydia@example.com").withAddress("little tokyo")
+            .withLessons(List.of(new Lesson("FIONA Lesson", SampleDataUtil.SAMPLE_DATE_TIME_6)))
+            .build();
     public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
-            .withEmail("anna@example.com").withAddress("4th street").build();
+            .withEmail("anna@example.com").withAddress("4th street")
+            .withLessons(List.of(new Lesson("GEORGE Lesson", SampleDataUtil.SAMPLE_DATE_TIME_7)))
+            .build();
 
     // Manually added
     public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
-            .withEmail("stefan@example.com").withAddress("little india").build();
+            .withEmail("stefan@example.com").withAddress("little india")
+            .withLessons(List.of(new Lesson("HOON Lesson", SampleDataUtil.SAMPLE_DATE_TIME_8)))
+            .build();
     public static final Person IDA = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
-            .withEmail("hans@example.com").withAddress("chicago ave").build();
+            .withEmail("hans@example.com").withAddress("chicago ave")
+            .withLessons(List.of(new Lesson("IDA Lesson", SampleDataUtil.SAMPLE_DATE_TIME_1)))
+            .build();
 
     // Manually added - Person's details found in {@code CommandTestUtil}
     public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
